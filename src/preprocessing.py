f09be95f45e9215ac56d50991e3a6f59cb9bb2d0
Luigi D. C. Soares <luigi.soares@sga.pucminas.br>
2019-06-08 21:33:54 -0300

    KModes (ou não)

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -4,13 +4,26 @@ def select(df, cols={}):
     """ Select data from dataframe filtering by the columns passed to this
     function and renaming them.
 
+    :param pandas.DataFrame df: The original dataframe.
     :param dict cols: The columns which will be selected and their respective
     names.
-    :param pandas.DataFrame df: The original dataframe.
-    :type file: str or None.
-    :return the new DataFrame.
+    :return the new dataframe.
     :rtype pandas.DataFrame.
     """
 
     selected = df[[k for k in cols]]
     return selected.rename(index=str, columns=cols)
+
+def discretize(df, col, bins, labels=None):
+    """ Discretize numeric columns accordingly with the number of bins passed
+    as param.
+
+    :param pandas.DataFrame df: The original dataframe.
+    :param string col: The column that will be discretized.
+    :param list bins: customized bins.
+    :param list labels: custom labels.
+    :return the new dataframe.
+    :rtype pandas.DataFrame.
+    """
+
+    return pd.cut(df[col], bins, False)
